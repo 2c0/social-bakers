f5d0d1957a0edfa4606a614db6822a14fafdeca0
Hai Ha Nguyen <haicorp87@gmail.com>
2019-07-08 00:29:43 +0200

    make the panel hidden by default

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -8,7 +8,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sidebarOpen: true
+      sidebarOpen: false
     };
     this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
   }
